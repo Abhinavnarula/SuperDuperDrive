e30753b31bc8ee88ee7fb665c288b1f85c80fe83
Abhinavnarula <abhinavnarula3@gmail.com>
2021-07-18 00:57:22 +0530

    changes in signing up

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/SignupController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/SignupController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller()
 @RequestMapping("/signup")
@@ -25,7 +26,7 @@ public class SignupController {
     }
 
     @PostMapping()
-    public String signupUserCredential(@ModelAttribute User user, Model model) {
+    public String signupUserCredential(@ModelAttribute User user, Model model, RedirectAttributes redirectAttributes) {
         String errorString = null;
 
         if (!userService.isUsernameAvailable(user.getUsername())) {
@@ -41,6 +42,9 @@ public class SignupController {
 
         if (errorString == null) {
             model.addAttribute("signupSuccess", true);
+            redirectAttributes.addFlashAttribute("SuccessMessage","Sign Up Successfully");
+                return "redirect:/login";
+
         } else {
             model.addAttribute("signupError", errorString);
         }
